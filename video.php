bd3d2d2f15a6e1cdb8675210b187936b95750264
NotMike <notmike@notmike.co.uk>
2023-03-05 10:55:01 +0000

    oops

--- video.php
+++ video.php
@@ -1,7 +1,7 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
 include("src/database.php");
 
 $Video = Database("SELECT Filename, Title, SHA256, UploadData FROM Videos WHERE ID = :ID", [':ID'=>$_GET['id']]);
@@ -14,7 +14,7 @@ session_start();
 if (!$_SESSION['videos'] || !$_SESSION['videos'][$Video['Filename']])
     die("Unauthorised");
 $fp = fopen("files/".$Video['Filename'], 'rb');
-//header("Content-Type: video/mp4");
+header("Content-Type: video/mp4");
 header("Content-Length: " . filesize("files/".$Video['Filename']));
 session_write_close();
 fpassthru($fp);
